fd556bb7f4ba95184250a18bc07b053f51f864ba
khanyisoluzipo <77772866+khanyisoluzipo@users.noreply.github.com>
2022-05-31 15:27:21 +0200

    Update Startup.cs

--- AddressBook.Api/Startup.cs
+++ AddressBook.Api/Startup.cs
@@ -28,7 +28,7 @@ namespace AddressBook.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<AddressDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DBConn")));
+            services.AddDbContext<AddressDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("ConnString")));
 
             services.AddScoped<IPersonRepository, PersonRepository>();
             services.AddScoped<ISuburbRepository, SuburbRepository>();
